59259cbf70d17adf19f910886c953c7b4419dc33
slvsh-mob <will@Wills-MacBook-Pro.local>
2020-06-11 19:48:05 -0400

    Final Push

---
+++ client/build/precache-manifest.5c89563161e59872a54ebf095190d16a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c419183141216b4a8121fc63c5b7e6c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe5c135e8959fd0d0671",
+    "url": "/static/css/main.3f05a96a.chunk.css"
+  },
+  {
+    "revision": "505e313a905f94fbf3d7",
+    "url": "/static/js/2.cbebe6cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cbebe6cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5c135e8959fd0d0671",
+    "url": "/static/js/main.23cb09ee.chunk.js"
+  },
+  {
+    "revision": "890bd89f311c22c5a985",
+    "url": "/static/js/runtime-main.62dfcb32.js"
+  }
+]);
