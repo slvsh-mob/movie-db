604b56004ccfa0d2d15aced55fa0a6d195b85868
slvsh-mob <will@Wills-MacBook-Pro.local>
2020-06-11 21:51:53 -0400

    Final Push

--- app.js
+++ app.js
@@ -17,15 +17,6 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedT
     console.log('connected to DB')
 )
 
-//For Deployment//Serve Static Assets from client/build//
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
-
 //Set Upload folder and specify middleware
 app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -50,6 +41,15 @@ app.use('/api/directors', directorRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/movies', movieRoutes)
 app.use('/api/comments', commentRoutes)
+
+//For Deployment//Serve Static Assets from client/build//
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 //Create General 404 Error
 app.use((req, res, next) => {
--- client/src/components/listview/listview.js
+++ client/src/components/listview/listview.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ListElement from './listviewElement'
 
 const ListView = (props) => {
-    console.log(props.input)
+    console.log(props.inlet)
     return(
-        props.input.map((index, value) => {
+        props.inlet.map((index, value) => {
             return(
                 <ListElement input={index}/>
             )
