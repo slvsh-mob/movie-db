c1922c150dbd2a98b9b1c22038bfcdaf09e47907
slvsh-mob <will@Wills-MacBook-Pro.local>
2020-06-23 22:40:19 -0400

    updates to login and signup

--- client/src/routes/login.js
+++ client/src/routes/login.js
@@ -2,6 +2,20 @@ import React from 'react'
 import axios from 'axios'
 import swal from 'sweetalert'
 
+//Photos
+import low from '../static_images/low.jpg'
+import tron from '../static_images/tron.jpg'
+import fal from '../static_images/fal.jpg'
+import pf from '../static_images/pf.jpg'
+import dk from '../static_images/dk.jpg'
+import sf from '../static_images/sf.jpg'
+import fc from '../static_images/fc.jpg'
+import esb from '../static_images/esb.jpg'
+import joker from '../static_images/joker.jpg'
+import avatar from '../static_images/avatar.jpg'
+import terminator from '../static_images/terminator.jpg'
+import vertigo from '../static_images/vertigo.jpg'
+
 const LoginPage = () => {
     const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
@@ -63,7 +77,24 @@ const LoginPage = () => {
         <div className="single_page_background">
             <div style={login_div}>
                 <div style={left_div}>
-
+                    <div style={row_1}>
+                        <div style={row_element}><img src={low} alt="poster missing" style={photo_style}/></div>
+                        <div style={row_element}><img src={dk} alt="poster missing" style={photo_style}/></div>
+                        <div style={row_element}><img src={fal} alt="poster missing" style={photo_style}/></div>
+                        <div style={row_element}><img src={pf} alt="poster missing" style={photo_style}/></div>
+                    </div>
+                    <div style={row_1}>
+                        <div style={row_element}><img src={fc} alt="poster missing" style={photo_style}/></div>
+                        <div style={row_element}><img src={sf} alt="poster missing" style={photo_style}/></div>
+                        <div style={row_element}><img src={esb} alt="poster missing" style={photo_style}/></div>
+                        <div style={row_element}><img src={joker} alt="poster missing" style={photo_style}/></div>
+                    </div>
+                    <div style={row_1}>
+                    <div style={row_element}><img src={avatar} alt="poster missing" style={photo_style}/></div>
+                        <div style={row_element}><img src={terminator} alt="poster missing" style={photo_style}/></div>
+                        <div style={row_element}><img src={vertigo} alt="poster missing" style={photo_style}/></div>
+                        <div style={row_element}><img src={tron} alt="poster missing" style={photo_style}/></div>
+                    </div>
                 </div>
                 <div style={right_div}>
                     <div style={title_row}>
@@ -71,17 +102,19 @@ const LoginPage = () => {
                     </div>
                     <div style={middle_row}>
                         <p style={middle_text}>Welcome to the Movie database</p>
-                        <p style={middle_text}>Please Sign In:</p>
                     </div>
                     <div style={input_row}>
                         <form style={form_style}>
-                            <input type="text" placeholder="Email" value={email} style={login_input_T} onChange={handleEmail}/>
-                            <input type="password" placeholder="Password" value={password} style={login_input_B} onChange={handlePassword}/>
+                            <label for="input_one">Username or Email</label>
+                            <input type="text" id="input_one" value={email} style={login_input_T} onChange={handleEmail}/>
+                            <label for="input_two">Password</label>
+                            <input type="password" id="input_two" value={password} style={login_input_B} onChange={handlePassword}/>
+                            <a href="/resetPW" style={lost_pw_style}>Forgot Password?</a>
                         </form>
                     </div>
                     <div style={lower_unit}>
                         <div style={btn_row}>
-                            <button style={submit_btn} onClick={handleSubmit}>Login</button>
+                            <button style={submit_btn} onClick={handleSubmit}>Sign In</button>
                         </div>
                         <div style={line_div}>
                             <hr style={line_style}></hr>
@@ -89,13 +122,30 @@ const LoginPage = () => {
                             <hr style={line_style}></hr>
                         </div>
                         <div style={btn_row}>
-                            <button style={submit_btn} onClick={() => window.location = "/signup"}>Signup</button>
+                            <p>New to the Movie Databse? <a href="/signup">Create an Account</a></p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     );
+}
+
+const lost_pw_style ={
+    textAlign: "right"
+}
+
+const signup_btn = {
+    backgroundColor: "#008ECC",
+    flexGrow: 1,
+    maxWidth: "300px",
+    fontSize: "20pt",
+    borderRadius: "5px"
+}
+
+const photo_style = {
+    height: "100%",
+    width: "100%"
 }
 
 const form_style = {
@@ -111,7 +161,7 @@ const lower_unit_text = {
 }
 
 const line_style = {
-    width: "45%",
+    width: "30%",
 }
 
 const line_div = {
@@ -131,10 +181,11 @@ const lower_unit = {
 
 const submit_btn = {
     flexGrow: 1,
-    maxWidth: "300px",
-    backgroundColor: "#27AE60",
+    maxWidth: "225px",
+    backgroundColor: "#696969",
     fontSize: "20pt",
-    borderRadius: "5px"
+    borderRadius: "25px",
+    color: "white"
 }
 
 const btn_row = {
@@ -147,7 +198,7 @@ const btn_row = {
 
 const login_div = {
     width: "80%",
-    height: "80%",
+    height: "750px",
     backgroundColor: "#EEEEEE",
     position: "fixed",
     zIndex: "2",
@@ -158,14 +209,31 @@ const login_div = {
 }
 
 const left_div = {
-    flexGrow: "1",
+    width: "650px",
     height: "100%",
-    backgroundColor: "#D65A31"
+    backgroundColor: "#D65A31",
+    display: "flex",
+    flexDirection: "column",
+    boxShadow: "1px 1px 5px gray"
+}
+
+const row_1 = {
+    height: "250px",
+    backgroundColor: "black",
+    display: "flex",
+    flexDirection: "row"
+}
+
+const row_element = {
+    width: "25%",
+    height: "100%",
+    backgroundColor: "#2C3E50",
+    border: "1px solid black"
 }
 
 const right_div = {
     minWidth: "470px",
-    maxWidth: "700px",
+    maxWidth: "900px",
     flexGrow: "1",
     height: "100%",
     display: "flex",
@@ -177,23 +245,29 @@ const right_div = {
 const login_input_B = {
     flexGrow: "1",
     margin: "0 0 10px 0",
-    height: "5vh",
-    fontSize: "3vh",
+    height: "20px",
+    fontSize: "20px",
     width: "400px",
-    textIndent: "10px"
+    textIndent: "10px",
+    background: "transparent",
+    border: "none",
+    borderBottom: "1px solid gray"
 }
 
 const login_input_T = {
     flexGrow: "1",
-    margin: "0 0 10px 0",
-    height: "5vh",
-    fontSize: "3vh",
+    margin: "0 0 20px 0",
+    height: "20px",
+    fontSize: "20px",
     width: "400px",
-    textIndent: "10px"
+    textIndent: "10px",
+    background: "transparent",
+    border: "none",
+    borderBottom: "1px solid gray"
 }
 
 const title_row = {
-    flexGrow: "2",
+    height: "225px",
     display: "flex",
     flexDirection: "row",
     justifyContent: "center"
@@ -202,23 +276,24 @@ const title_row = {
 const login_title = {
     flexGrow: "1",
     fontSize: "6vh",
-    fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
+    fontFamily: "Krona One"
 }
 
 const middle_row = {
-    flexGrow: "2",
+    height: "100px",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
 }
 
 const middle_text = {
-    fontSize: "2vh",
+    fontSize: "35px",
     lineHeight: "4vh",
     textAlign: "center",
-    margin: "0 0 10px 0"
+    margin: "0 0 10px 0",
+    fontFamily: "PT Sans Narrow"
 }
 
 const input_row = {
